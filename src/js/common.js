4987194aab471f6465b62d6c453d3969fcf12348
huangjianxing <15123327139@163.com>
2024-09-18 23:58:28 +0800

    feat：分类树

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,45 @@
 
+function expandCategoryTree (tree) {
+  for (var i = 0; i < tree.length ; i++) {  
+    // console.log(tree [i])
+    var $li = $(tree [i])
+    // 获取a元素
+    var $category =$li.children().eq(0)
+    var cur_href = $category.attr('href')
+    var is_href = false
+    const { href, pathname } = location
+    // console.log('href, pathname, cur_href:',href ,cur_href)
+    if (pathname === cur_href) {
+      // 当前连接为该分类，若存在ul，这展开该ul
+      // console.log('找到连接:href, pathname, cur_href:',href,pathname ,cur_href)
+      is_href = true
+    }
+    // 是否存在ul
+    var $ulElements = $li.children('ul')
+    // console.log('$ulElements:',$ulElements)
+    if(is_href) {
+      if($ulElements.length > 0) {
+        // 分类找到且存在子菜单则展开
+        console.log('cur_href,cur----ulElements:',cur_href,$ulElements)
+        $category.addClass('arrow-dropdown')
+        $ulElements.addClass('expand')
+      }
+      console.log('cur_href,cur----category:',cur_href,$category)
+      // 选择当前分类
+      $category.addClass('current')
+      return true
+    }else if($ulElements.length > 0) {
+      // 分类未找到且存在ul则继续递归找
+      if(expandCategoryTree($ulElements.children())) {
+        $ulElements.addClass('expand')
+        $category.addClass('arrow-dropdown')
+        console.log('未找到分类：cur_href,cur----$category:',cur_href,$category)
+        return true
+      }
+    }
+  }
+  return false
+}
 
 const commonContext = {
   // testaaa() {
@@ -69,14 +110,22 @@ const commonContext = {
   //     }
   //   })
   // }
+  /** 分类树初始化 */
+  initCategoryTree() {
+    const $categories_Tree = $('.card.widget.categories > ul > li')
+    // console.log('$categories_Tree:', $categories_Tree)
+    expandCategoryTree($categories_Tree)
+  }
 }
 
-
-window.commonContext = commonContext
+// 在现代JavaScript开发中，我们倾向于使用模块来封装代码，以保持全局命名空间的清洁和避免命名冲突。
+// 然而，在某些情况下，你可能需要让某个函数在全局作用域中可用，以便它可以被来自不同模块或第三方库的代码访问。
+// 这时，将函数赋值给window对象是一个可行的解决方案。
+// window.commonContext = commonContext
 
 !(function () {
   // 若没有将common放到body最后，需要再html加载完后执行菜单高亮才行
-  const loads = ['initCarousel', 'sparkInput', 'websiteTime','initNavbar']
+  const loads = ['initCarousel', 'sparkInput', 'websiteTime','initNavbar','initCategoryTree']
   const omits = ['initEffects', 'loadMaintain', 'showThemeVersion']
 
   Object.keys(commonContext).forEach(
